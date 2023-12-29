cd27722320623c37ddda45c55265797a2dd9fd33
Imran Ansari <askimranansari@gmail.com>
2023-12-29 21:08:35 +0530

    last

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,66 +1,44 @@
-import { useEffect, useState } from "react";
-import "../styles/home.css";
-import Movielist from "./Movielist";
-import { Sliderify } from "react-sliderify";
+import { useEffect, useState } from 'react';
+import '../styles/home.css'
+import Movielist from './Movielist';
+import { Sliderify } from "react-sliderify";    
+
 
 const Home = () => {
-  let [movies, setMovies] = useState(null);
-  let [sliderMovies, setsliderMovies] = useState(null);
 
-  useEffect(() => {
-    fetch("https://dev-3r7bl6u2pv306hj.api.raw-labs.com/movies")
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data);
-        setsliderMovies(data.slice(0, 5));
-      });
-  }, []);
+    let[movies , setMovies] = useState(null);
+    let[sliderMovies , setsliderMovies] = useState(null);
 
-  return (
-    <div className="home-comp">
-      {sliderMovies && (
-        <Sliderify
-          style={{ width: "800px" }}
-          slideDurationInSecs="3"
-          showSpot={false}
-          rounded={true}
-        >
-          {sliderMovies.map((m) => {
-            return (
-              <div
-                style={{
-                  height: "500px",
-                  background: `url(${m.banner})`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
-                }}
-              ></div>
-            );
-          })}
-        </Sliderify>
-      )}
+    useEffect(()=>{
+        fetch("http://localhost:4000/movies")
+        .then(res=>res.json())
+        .then((data)=>{
+            setMovies(data);
+            setsliderMovies(data.slice(0,5))
+        })
+    } , [])
 
-      {movies && <Movielist movies={movies} title="All movies" />}
 
-      {movies && (
-        <Movielist
-          movies={movies.filter((m) => {
-            return m.languages.includes("English");
-          })}
-          title="Hollywood movies"
-        />
-      )}
+    return ( <div className="home-comp">
 
-      {movies && (
-        <Movielist
-          movies={movies.filter((m) => {
-            return m.rating >= 9;
-          })}
-          title="Top rated movies"
-        />
-      )}
-    </div>
-  );
-};
+           {sliderMovies && 
+           <Sliderify style={{width:"800px"}} slideDurationInSecs="3" showSpot={false} rounded={true}>
+            {
+                sliderMovies.map((m)=>{ return(
+                    <div style={{height:"500px",background:`url(${m.banner})`,backgroundRepeat:"no-repeat",backgroundSize:"cover" }}>
+                    </div>
+                )})
+            }
+            </Sliderify>}
 
+               {movies && <Movielist movies={movies} title="All movies"/>}
+
+               {movies && <Movielist movies={movies.filter((m)=>{return m.languages.includes("English")})} title="Hollywood movies"/>}
+
+               {movies && <Movielist movies={movies.filter((m)=>{return m.rating>=9})} title="Top rated movies"/>}
+
+
+            </div> );
+}
+ 
 export default Home;
